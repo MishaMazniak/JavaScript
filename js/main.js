4027aadf5a650a11b1e291aa5d5edf39d2ee92b3
MishaMazniak <Mishamaznyak@gmail.com>
2023-01-18 12:05:25 +0100

    add failse

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var text = document.getElementById('text').addEventListener('input', select);
+var sel = "";
+var num = 0;
+function select(event){
+  var x = event.target.value;
+  sel = x;
+
+  if(sel == "Audi")
+    num = 3;
+  else if(sel == "BMW")
+      num = 1;
+  else if(sel == "Mersedes")
+      num = 2;
+  else sel = "";
+console.log(sel);
+console.log(num);
+
+}
