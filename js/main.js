a7fc5deacf66440403e719440c83f56b9c99c404
MishaMazniak <Mishamaznyak@gmail.com>
2023-01-18 12:23:59 +0100

    change new text

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-var text = document.getElementById('text').addEventListener('input', select);
+var text = document.getElementById("text").addEventListener("input", select);
 var sel = "";
 var num = 0;
-function select(event){
+function select(event) {
   var x = event.target.value;
   sel = x;
 
-  if(sel == "Audi")
-    num = 3;
-  else if(sel == "BMW")
-      num = 1;
-  else if(sel == "Mersedes")
-      num = 2;
+  if (sel == "Audi") num = 3;
+  else if (sel == "BMW") num = 1;
+  else if (sel == "Mersedes") num = 2;
   else sel = "";
-console.log(sel);
-console.log(num);
-
+  console.log(sel);
+  console.log(num);
+  console.log("New text");
 }
